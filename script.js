fcae93db68309426a3b424df1eeefdc20d4fd4e5
SadiaAfrinMim <mimsadia367@gmail.com>
2024-10-05 01:30:28 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const loadAllPhones = async(status,brandName) =>{
+  document.getElementById('spinner').style.display = 'none'
+const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:"iphone"}`)
+const data = await res.json()
+if(status){
+  displayAllPhones(data.data)
+
+}
+else{
+  displayAllPhones(data.data.slice(0, 6))
+
+}
+
+
+
+}
+const  handleShowAll=()=>{
+  loadAllPhones(true)
+
+}
+
+const displayAllPhones=(phones)=>{
+  const phoneContainer = document.getElementById('phones-container')
+  phones.forEach(element => {
+    const{image,slug,brand}=element
+    console.log(element);
+    const div = document.createElement('div')
+    div.innerHTML=`
+    <div class="card bg-base-100 w-96 pt-8 shadow-xl">
+  <figure>
+    <img
+      src=${image}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${brand}</h2>
+    <p>${slug}</p>
+    <div class="card-actions justify-end">
+      <button onclick ="phoneDetails('${slug}')" class="btn btn-primary">Show details</button>
+    </div>
+  </div>
+</div>`
+phoneContainer.appendChild(div)
+    
+  });
+  console.log(phones);
+}
+
+const handleSearch=()=>{
+  document.getElementById('spinner').style.display ='block'
+  const searchText = document.getElementById('search-box').value
+  setTimeout(() => {
+    loadAllPhones(false,searchText)
+  }, 3000);
+}
+
+const phoneDetails =async(slugs)=>{
+ const response =await fetch(` https://openapi.programming-hero.com/api/phone/${slugs}`)
+ const data = await response.json()
+ console.log(data.data);
+ const{image,slug,brand}= data.data
+ const modalcontainer = document.getElementById('modal-container')
+ 
+ modalcontainer.innerHTML = `
+ <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">${brand}</h3>
+      <p class="py-4">${slug}</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+ `
+ 
+ my_modal_1.showModal()
+
+}
+
+loadAllPhones(false,"iphone")
